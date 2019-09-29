51d9c5748e7876b480ade6cc9b81b58b7fa62edc
helgaParis <helga@artistessansfrontieres.fr>
2019-09-29 13:52:45 +0200

    js with urls of diaporamas

---
+++ files/photos/implosion/dias-listes.js
@@ -0,0 +1,59 @@
+//url list, one list per show.
+
+var photos=[
+    "url('/photos/implosion/1.jpg')",
+    "url('/photos/implosion/2.jpg')",
+    "url('/photos/implosion/3.jpg')",
+    "url('/photos/implosion/4.jpg')",
+    "url('/photos/implosion/5.jpg')",
+    "url('/photos/implosion/6.jpg')",
+    "url('/photos/implosion/7.jpg')",
+    "url('/photos/implosion/8.jpg')",
+    "url('/photos/implosion/9.jpg')",
+    "url('/photos/implosion/10.jpg')",
+    "url('/photos/implosion/11.jpg')",
+    "url('/photos/implosion/12.jpg')",
+    "url('/photos/implosion/13.jpg')",
+    "url('/photos/implosion/14.jpg')",
+    "url('/photos/implosion/15.jpg')",
+    "url('/photos/implosion/16.jpg')",
+    "url('/photos/implosion/17.jpg')",
+    "url('/photos/implosion/18.jpg')",
+    "url('/photos/implosion/19.jpg')",
+    "url('/photos/implosion/20.jpg')",
+    "url('/photos/implosion/21.jpg')",
+    "url('/photos/implosion/22.jpg')",
+    "url('/photos/implosion/23.jpg')",
+    "url('/photos/implosion/24.jpg')",
+    "url('/photos/implosion/25.jpg')"
+];
+// sans titre: mettre ''
+var legendes = [
+    'Implosion 1',
+    'Implosion 2',
+    'Implosion 3',
+    'Implosion 4',
+    'Implosion 5',
+    'Implosion 6',
+    'Implosion 7',
+    'Implosion 8',
+    'Implosion 9',
+    'Implosion 10',
+    'Implosion 11',
+    'Implosion 12',
+    'Implosion 13',
+    'Implosion 14',
+    'Implosion 15',
+    'Implosion 16',
+    'Implosion 17',
+    'Implosion 18',
+    'Implosion 19',
+    'Implosion 20',
+    'Implosion 21',
+    'Implosion 22',
+    'Implosion 23',
+    'Implosion 24',
+    'Implosion 25'
+];
+
+var delai = 2000; // delai between pictures in ms
---
+++ files/photos/moab/dias-listes.js
@@ -0,0 +1,61 @@
+//url list, one list per show.
+
+var photos=[
+    "url('/photos/moab/1.jpg')",
+    "url('/photos/moab/2.jpg')",
+    "url('/photos/moab/3.jpg')",
+    "url('/photos/moab/4.jpg')",
+    "url('/photos/moab/5.jpg')",
+    "url('/photos/moab/6.jpg')",
+    "url('/photos/moab/7.jpg')",
+    "url('/photos/moab/8.jpg')",
+    "url('/photos/moab/9.jpg')",
+    "url('/photos/moab/10.jpg')",
+    "url('/photos/moab/11.jpg')",
+    "url('/photos/moab/12.jpg')",
+    "url('/photos/moab/13.jpg')",
+    "url('/photos/moab/14.jpg')",
+    "url('/photos/moab/15.jpg')",
+    "url('/photos/moab/16.jpg')",
+    "url('/photos/moab/17.jpg')",
+    "url('/photos/moab/18.jpg')",
+    "url('/photos/moab/19.jpg')",
+    "url('/photos/moab/20.jpg')",
+    "url('/photos/moab/21.jpg')",
+    "url('/photos/moab/22.jpg')",
+    "url('/photos/moab/23.jpg')",
+    "url('/photos/moab/24.jpg')",
+    "url('/photos/moab/25.jpg')",
+    "url('/photos/moab/26.jpg')"
+];
+// sans titre: mettre ''
+var legendes = [
+    'Voyage sur Mars - Moab',
+    'Voyage sur Mars - Moab 2',
+    'Eros sur mars - Moab 3',
+    'Green river',
+    'Green river 2',
+    'Green river 3',
+    'Green river 4',
+    'Eau sur mars - Green river',
+    'Moab 4',
+    'Moab 5',
+    'Moab 6',
+    'Pierres et touristes - Moab',
+    'Un peu de vie - Moab',
+    'Dessert en coulisses de film',
+    'Nids de pies',
+    'Dessert en montagne',
+    'Printemps',
+    'Quête d\'eau',
+    'Pas d\'eau',
+    'Pas assez d\'eau',
+    'Un autre année sèche',
+    'Fairview',
+    'Salt Lake City',
+    'La vallée de Salt Lake',
+    'La vallée de Salt Lake 2',
+    'Salt Lake City au soir'
+];
+
+var delai = 4000; // delai between pictures in ms
